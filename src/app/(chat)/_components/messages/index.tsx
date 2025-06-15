ed0691e96e8d86fd33fbff97c61db32bbc13ee33
Jason <jason@hedmans.org>
2025-06-14 22:25:35 -0400

    messages padding

--- src/app/(chat)/_components/messages/index.tsx
+++ src/app/(chat)/_components/messages/index.tsx
@@ -35,7 +35,7 @@ const PureMessages: React.FC<Props> = ({
   return (
     <div
       ref={containerRef}
-      className="relative mb-4 flex h-full min-w-0 flex-1 flex-col gap-6 overflow-y-scroll"
+      className="relative flex h-full min-w-0 flex-1 flex-col gap-6 overflow-y-scroll py-8"
     >
       {messages.map((message, index) => (
         <PreviewMessage
