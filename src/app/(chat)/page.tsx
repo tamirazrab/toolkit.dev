914ced3a3cfce5db19186dd61f72b0c34038178f
Jason <jason@hedmans.org>
2025-06-16 03:37:44 -0400

    hotfix infinite redirect

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -1,10 +1,8 @@
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 import { Chat } from "./_components/chat";
 
 import { auth } from "@/server/auth";
-import { api } from "@/trpc/server";
 
 import { generateUUID } from "@/lib/utils";
 
@@ -13,14 +11,6 @@ export default async function Page() {
 
   if (!session) {
     return null;
-  }
-
-  // Check if this is a new user by looking at their chat history
-  const chatHistory = await api.chats.getChats({ limit: 1 });
-  
-  // If user has no chat history, redirect to onboarding
-  if (chatHistory.items.length === 0) {
-    redirect("/onboarding");
   }
 
   const id = generateUUID();
