10a2f4944a145ecc91a6a89ec5dfa4555451b882
Jason <jason@hedmans.org>
2025-07-21 16:54:32 -0400

    update toolkit card

--- src/app/_components/landing-page/top-toolkits/toolkit-card.tsx
+++ src/app/_components/landing-page/top-toolkits/toolkit-card.tsx
@@ -29,10 +29,16 @@ export const ToolkitCard: React.FC<ToolkitCardProps> = ({ toolkit, count }) => {
         "lg:border-r lg:nth-[3n]:border-r-0 lg:nth-last-[3]:border-b-0",
       )}
     >
-      <VStack className="items-start gap-2">
-        <HStack className="gap-4">
-          <ToolkitIcon toolkit={toolkit} className="size-5 md:size-6" />
-          <h3 className="text-lg font-bold">{clientToolkit.name}</h3>
+      <VStack className="w-full items-start gap-2">
+        <HStack className="w-full justify-between">
+          <HStack className="gap-4">
+            <ToolkitIcon toolkit={toolkit} className="size-5 md:size-6" />
+            <h3 className="text-lg font-bold">{clientToolkit.name}</h3>
+          </HStack>
+          <HStack className="gap-1">
+            <Activity className="size-3" />
+            <p>{count}</p>
+          </HStack>
         </HStack>
         <p className="text-muted-foreground text-sm">
           {clientToolkit.description}
@@ -42,10 +48,6 @@ export const ToolkitCard: React.FC<ToolkitCardProps> = ({ toolkit, count }) => {
         <HStack className="gap-1">
           <Wrench className="size-3" />
           <p>{Object.keys(clientToolkit.tools).length} tools</p>
-        </HStack>
-        <HStack className="gap-1">
-          <Activity className="size-3" />
-          <p>{count} invocations</p>
         </HStack>
       </HStack>
     </VStack>
