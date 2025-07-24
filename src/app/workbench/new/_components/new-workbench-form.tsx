4d8b3779ef526b9017fdb1c08aebe0a6c8d6c676
Jason <jason@hedmans.org>
2025-07-24 12:04:36 -0400

    update new workbench form

--- src/app/workbench/new/_components/new-workbench-form.tsx
+++ src/app/workbench/new/_components/new-workbench-form.tsx
@@ -167,8 +167,8 @@ export function NewWorkbenchForm() {
                     )}
                   </Button>
                 </DialogTrigger>
-                <DialogContent className="max-h-[80vh] max-w-lg gap-2 overflow-hidden">
-                  <DialogHeader className="gap-1">
+                <DialogContent className="h-[90vh] max-w-lg gap-2 overflow-hidden p-1 md:h-[60vh] md:p-3">
+                  <DialogHeader className="gap-1 px-2 pt-2">
                     <DialogTitle>Select Toolkits</DialogTitle>
                     <DialogDescription>
                       Choose the toolkits that will be available in this
